0c2a3f2a941ac0f0b2f36b16feb89ae1a6d984a8
Suraj Kumar <surajsinhaking77@gmail.com>
2024-01-23 20:52:55 +0530

    done

--- server/index.js
+++ server/index.js
@@ -1,20 +1,18 @@
 const express = require('express');
 const http = require('http');
 const database_connectivity = require('./config/dbconfig');
-const PORT = 8000;
 const cors = require('cors');
 require('dotenv').config();
 
 const app = express();
-
 
 //Global Middleware
 app.use(express.json());
 app.use(cors());
 
 
-database_connectivity();
+database_connectivity(process.env.MongoDBuRL);
 
-app.listen(PORT, ()=>{
+app.listen(process.env.PORT, ()=>{
     console.log("The server is Running on http://localhost:"+PORT);
 })
